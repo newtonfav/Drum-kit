d6a4d53b7a9a127bfd21a92c7fc8cf8242297e53
Favour Oghenekowho <gh.oghene@gmail.com>
2022-07-04 01:08:27 +0100

    changed snare

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("keydown", (e) => {
     console.log(e.code);
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
     if(!audio) return;
- 
+    audio.currentTime = 0;
     audio.play();
 })
