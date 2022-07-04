5a8b75d90b7402ddd59464bcf2c9256ad3aa4a19
Favour Oghenekowho <gh.oghene@gmail.com>
2022-07-04 16:24:59 +0100

    logo

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function soundPlay(e) {
-    console.log(e.code);
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
     if(!audio) return; // Do nothing
     audio.currentTime = 0;
     audio.play();
 }
-document.addEventListener("keydown", soundPlay);
+document.addEventListener("keydown", soundPlay);    
