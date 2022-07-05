638de324b0e2de2e519fc7c82c76b8d5d950df3a
Favour Oghenekowho <gh.oghene@gmail.com>
2022-07-05 23:10:16 +0100

    addded transition

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function soundPlay(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`div[data-key="${e.code}"]`)
     if(!audio) return; // Do nothing
     audio.currentTime = 0;
     audio.play();
+    key.classList.add("playing");
 }
+
+function removeTransition(e) {
+    if(e.propertyName !== "transform") return; 
+    this.classList.remove("playing");
+}
+
+
+const drumSet = document.querySelectorAll(".circle"); 
+drumSet.forEach( key => key.addEventListener("transitionend", removeTransition));
 document.addEventListener("keydown", soundPlay);    
